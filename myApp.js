7fecde89a6126866601f92f98bb95b0b3fcfd58a
Haikal Alfandi <kungkingkangcanyouseemekuking@gmail.com>
2023-10-23 01:03:12 -0400

    connecct to mongoDB

--- myApp.js
+++ myApp.js
@@ -1,5 +1,10 @@
 require('dotenv').config();
 
+// connect to mongoose database
+let mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
 
 let Person;
 
